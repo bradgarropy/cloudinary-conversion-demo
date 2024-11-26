9cafdf6f69f35fcf089e3a3f5f6076b1a76a20d6
Brad Garropy <bradgarropy@gmail.com>
2024-11-26 10:47:54 -0600

    done.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import {sayHello} from "./hello"
+// 1. Go to https://cloudinary.com
+// 2. Click 'Sign Up For Free'
+// 3. Click 'Sign Up With Email'
+// 4. Show the media library
+// 5. Open up the URL (https://res.cloudinary.com/bradgarropy/image/upload/cloudinary-conversion-demo/austin)
+// 6. Transform it to WebP (https://res.cloudinary.com/bradgarropy/image/upload/f_webp/cloudinary-conversion-demo/austin)
 
-sayHello()
+import {v2 as cloudinary} from "cloudinary"
+import "dotenv/config"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+})
+
+const url = cloudinary.url("cloudinary-conversion-demo/austin", {
+    // fetch_format: "webp",
+})
+
+console.log(url)
